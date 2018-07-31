b24c1e8f90370b0f4d4d130a46bf14686c3c12c4
Vincent Toubiana <vincent.toubi@gmail.com>
2018-08-01 00:36:44 +0200

    First commit

---
+++ qookie.js
@@ -0,0 +1,16 @@
+
+function add_button(evt) {
+		if ( evt.target.firstChild.className === 'qc-cmp-ui-container') {
+			var qookie_div = $('#qcCmpButtons');
+			if ( qookie_div.length === 1) {
+				var qfix_button = $('#qcCmpButtonQookieFix');
+				if ( qfix_button.length === 0) {
+					var $new_button = $('<button/>').attr({ id:"qcCmpButtonQookieFix", name:'qc-cmp-button', class:'qc-cmp-button', onclick:'window.__cmpui("setAndSaveAllConsent",!1)'}).text("Je refuse");
+					qookie_div.append($new_button);
+				}
+			} 
+		}
+}
+
+
+document.body.addEventListener('DOMSubtreeModified',add_button);
