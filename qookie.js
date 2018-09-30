703c7dbae333181ec2b01c880a109b0a009ff6b0
Axel Leroy <blackeco@free.fr>
2018-09-30 20:25:16 +0200

    Added automatic refusal of consent as an option.

--- qookie.js
+++ qookie.js
@@ -17,7 +17,7 @@ function addButton(mutations) {
     const qookieDiv = document.getElementById("qcCmpButtons");
 	if (qookieDiv) {
 	  const qfixButton = document.getElementById("qcCmpButtonQookieFix");
-	  const qfixOptOutButton = document.getElementsByClassName("qc-cmp-button qc-cmp-secondary-button"); //Test if the publisher already includes an opt-out 
+	  const qfixOptOutButton = document.getElementsByClassName("qc-cmp-button qc-cmp-secondary-button"); //Test if the publisher already includes an opt-out
 	  if (!qfixButton && !qfixOptOutButton.length) {
 		const newButton = document.createElement("button");
 		newButton.textContent = api.i18n.getMessage("refuseText");
@@ -26,10 +26,19 @@ function addButton(mutations) {
 		newButton.className = "qc-cmp-button";
 		newButton.setAttribute("onclick", 'window.__cmpui("setAndSaveAllConsent",!1)');
 		qookieDiv.appendChild(newButton);
-		
+
 		if ( hasAlreadyAnswered() ) {
 			newButton.click();
-		} 
+		} else {
+      api.storage.sync.get((result) => {
+        const autoDiscardEnabled = result.auto_discard_enabled || false;
+        const autoDiscardDelay = (result.auto_discard_delay || 5) * 1000;
+        if (autoDiscardEnabled) {
+          new Promise((resolve) => setTimeout(resolve, autoDiscardDelay))
+          .then(() => newButton.click());
+        }
+      });
+    }
 	  }
 	}
   }
