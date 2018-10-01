c5bf289f4ae0d23532d25316487e64d093ea798c
Vincent Toubiana <vincent.toubi@gmail.com>
2018-10-01 07:42:06 +0200

    Prevent cookie banner from re-appearing
    Check if there already is a "pubconsentcookie", if so, click on opt-out
    when the banner shows up.

--- qookie.js
+++ qookie.js
@@ -5,23 +5,33 @@ if (chrome == undefined) {
     api = chrome;
 }
 
+function hasAlreadyAnswered() {
+	return (document.cookie.indexOf('pubconsent=') > 0)
+}
+
+
+
 function addButton(mutations) {
   const qookiePopupShown = mutations.some(mutation => mutation.target.firstChild && mutation.target.firstChild.classList && mutation.target.firstChild.classList.contains("qc-cmp-ui-container"))
   if (qookiePopupShown) {
     const qookieDiv = document.getElementById("qcCmpButtons");
-    if (qookieDiv) {
-      const qfixButton = document.getElementById("qcCmpButtonQookieFix");
+	if (qookieDiv) {
+	  const qfixButton = document.getElementById("qcCmpButtonQookieFix");
 	  const qfixOptOutButton = document.getElementsByClassName("qc-cmp-button qc-cmp-secondary-button"); //Test if the publisher already includes an opt-out 
-      if (!qfixButton && !qfixOptOutButton.length) {
-        const newButton = document.createElement("button");
-        newButton.textContent = api.i18n.getMessage("refuseText");
-        newButton.id = "qcCmpButtonQookieFix";
-        newButton.name = "qc-cmp-button";
-        newButton.className = "qc-cmp-button";
-        newButton.setAttribute("onclick", 'window.__cmpui("setAndSaveAllConsent",!1)');
-        qookieDiv.appendChild(newButton);
-      }
-    }
+	  if (!qfixButton && !qfixOptOutButton.length) {
+		const newButton = document.createElement("button");
+		newButton.textContent = api.i18n.getMessage("refuseText");
+		newButton.id = "qcCmpButtonQookieFix";
+		newButton.name = "qc-cmp-button";
+		newButton.className = "qc-cmp-button";
+		newButton.setAttribute("onclick", 'window.__cmpui("setAndSaveAllConsent",!1)');
+		qookieDiv.appendChild(newButton);
+		
+		if ( hasAlreadyAnswered() ) {
+			newButton.click();
+		} 
+	  }
+	}
   }
 };
 
